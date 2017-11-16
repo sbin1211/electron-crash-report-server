32527bdaf74ef59322b7c35183e38943181079e9
john muhl <git@johnmuhl.com>
2017-11-16 13:44:05 -0600

    chore(package): update eslint-config-oz to version 1.1.0

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
 }
 
-// FIXME: hapi-basic-auth isn't ready for hapi 17
-// const Basic = require('hapi-auth-basic')
+const Basic = require('hapi-auth-basic')
 const Boom = require('boom')
 const Handlebars = require('handlebars')
 const Hapi = require('hapi')
@@ -20,136 +19,138 @@ const server = new Hapi.Server({
 	router: {stripTrailingSlash: true},
 })
 
-server.route({
-	method: GET,
-	path: '/',
-	config: {
-		handler: async (request, h) => {
-			const sql = 'SELECT * FROM reports ORDER BY created_at DESC'
-
-			try {
-				const reports = await server.app.db.run(sql)
-
-				return h.view('index', {reports, title: 'crash reports'})
-			} catch (error) {
-				throw error
-			}
-		},
-	},
-})
-
-server.route({
-	method: POST,
-	path: '/',
-	handler: async (request, h) => {
-		if (request.payload) {
-			const payload = Object.assign({}, request.payload)
-			const file = payload.upload_file_minidump
-
-			delete payload.upload_file_minidump
-
-			try {
-				const report = await server.app.db.reports.saveDoc(payload)
-
-				await server.app.db.dumps.insert({file, report_id: report.id})
-
-				return {}
-			} catch (error) {
-				throw error
-			}
-		} else {
-			return Boom.badRequest()
-		}
-	},
-})
-
-server.route({
-	method: GET,
-	path: '/reports/{id}',
-	config: {
-		handler: async (request, h) => {
-			const id = Number(request.params.id)
-
-			try {
-				const report = await server.app.db.reports.find(id)
-
-				report.body = JSON.stringify(report.body, null, 2)
-
-				return h.view('report', {report, title: 'crash report'})
-			} catch (error) {
-				throw error
-			}
-		},
-	},
-})
-
-server.route({
-	method: DELETE,
-	path: '/reports/{id}',
-	config: {
-		handler: async (request, h) => {
-			const id = Number(request.params.id)
-
-			try {
-				await server.app.db.dumps.destroy({report_id: id})
-				await server.app.db.reports.destroy({id})
-
-				return {}
-			} catch (error) {
-				throw error
-			}
-		},
-	},
-})
-
-server.route({
-	method: GET,
-	path: '/reports/{id}/dump',
-	config: {
-		handler: async (request, h) => {
-			const id = Number(request.params.id)
-
-			try {
-				const dump = await server.app.db.dumps.findOne({report_id: id})
-				const name = `crash-${id}.dmp`
-
-				return h
-					.response(dump.file)
-					.header('content-disposition', `attachment; filename=${name}`)
-					.type('application/x-dmp')
-			} catch (error) {
-				throw error
-			}
-		},
-	},
-})
-
 async function main () {
 	try {
 		const db = await massive(process.env.DATABASE_URL)
 
-		await db.run(require('./sql/create-reports.js'))
-		await db.run(require('./sql/create-dumps.js'))
-		await db.run(require('./sql/add-open-to-reports.js'))
-		await db.run(require('./sql/add-closed_at-to-reports.js'))
+		// Prepare and update database if necessary
+		await db.run(require('./sql.js'))
 
+		// Load and configure plugins
 		await server.register({plugin: require('vision')})
 
-		server.app.db = db
+		// server.app.db = db
 
-		server.views({
-			engines: {html: Handlebars},
-			relativeTo: __dirname,
-			layout: true,
-			path: 'views',
-		})
+		// server.views({
+		//  engines: {html: Handlebars},
+		//  relativeTo: __dirname,
+		//  layout: true,
+		//  path: 'views',
+		// })
 
 		await server.start()
+
+		console.log(`Server running at: ${server.info.uri}`)
+
+		return server
 	} catch (error) {
+		console.error(error)
 		throw error
 	}
-
-	console.log(`Server running at: ${server.info.uri}`)
 }
+
+// server.route({
+//  method: GET,
+//  path: '/',
+//  config: {
+//    handler: async (request, h) => {
+//      const sql = 'SELECT * FROM reports ORDER BY created_at DESC'
+
+//      try {
+//        const reports = await server.app.db.run(sql)
+
+//        return h.view('index', {reports, title: 'crash reports'})
+//      } catch (error) {
+//        throw error
+//      }
+//    },
+//  },
+// })
+
+// server.route({
+//  method: POST,
+//  path: '/',
+//  handler: async (request, h) => {
+//    if (request.payload) {
+//      const payload = Object.assign({}, request.payload)
+//      const file = payload.upload_file_minidump
+
+//      delete payload.upload_file_minidump
+
+//      try {
+//        const report = await server.app.db.reports.saveDoc(payload)
+
+//        await server.app.db.dumps.insert({file, report_id: report.id})
+
+//        return {}
+//      } catch (error) {
+//        throw error
+//      }
+//    } else {
+//      return Boom.badRequest()
+//    }
+//  },
+// })
+
+// server.route({
+//  method: GET,
+//  path: '/reports/{id}',
+//  config: {
+//    handler: async (request, h) => {
+//      const id = Number(request.params.id)
+
+//      try {
+//        const report = await server.app.db.reports.find(id)
+
+//        report.body = JSON.stringify(report.body, null, 2)
+
+//        return h.view('report', {report, title: 'crash report'})
+//      } catch (error) {
+//        throw error
+//      }
+//    },
+//  },
+// })
+
+// server.route({
+//  method: DELETE,
+//  path: '/reports/{id}',
+//  config: {
+//    handler: async (request, h) => {
+//      const id = Number(request.params.id)
+
+//      try {
+//        await server.app.db.dumps.destroy({report_id: id})
+//        await server.app.db.reports.destroy({id})
+
+//        return {}
+//      } catch (error) {
+//        throw error
+//      }
+//    },
+//  },
+// })
+
+// server.route({
+//  method: GET,
+//  path: '/reports/{id}/dump',
+//  config: {
+//    handler: async (request, h) => {
+//      const id = Number(request.params.id)
+
+//      try {
+//        const dump = await server.app.db.dumps.findOne({report_id: id})
+//        const name = `crash-${id}.dmp`
+
+//        return h
+//          .response(dump.file)
+//          .header('content-disposition', `attachment; filename=${name}`)
+//          .type('application/x-dmp')
+//      } catch (error) {
+//        throw error
+//      }
+//    },
+//  },
+// })
 
 main()
