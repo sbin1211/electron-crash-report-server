65518f6478e088f90cbba0d0cd161d20ef7934b6
john muhl <git@johnmuhl.com>
2018-07-17 08:46:02 -0500

    🔥 body-parser

--- app/server.js
+++ app/server.js
@@ -1,13 +1,11 @@
 import "dotenv/config";
 import App from "./app.html";
-import bodyParser from "body-parser";
 import compression from "compression";
 import express from "express";
 import massive from "massive";
 import { routes } from "./manifest/server.js";
 import sapper from "sapper";
 import sirv from "sirv";
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { Store } from "svelte/store.js";
 
 const server = express();
@@ -24,7 +22,7 @@ const store = () =>
 massive(process.env.DATABASE_URL)
   .then(db => {
     server.set("db", db);
-    server.use(bodyParser());
+    server.use(express.json());
     server.use(compression({ threshold: 0 }));
     server.use(sirv("assets"));
     server.use(sapper({ App, routes, store }));
